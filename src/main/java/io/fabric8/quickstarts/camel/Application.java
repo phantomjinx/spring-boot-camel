d6ca0f4b9c8b6e0033f50df5bf89086f937125a5
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2023-09-04 14:59:47 +0100

    Increase timeout period of application

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -34,7 +34,7 @@ public class Application extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer://foo?period=5000")
+        from("timer://foo?period=500000")
             .setBody().constant("Hello World")
             .log(">>> ${body}");
     }
